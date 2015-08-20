06e39652534a9fe9b95b8f23c1216423f2d2aaad
Andy <php360@qq.com>
2015-08-20 14:48:47 +0800

    去除zend opcache

--- scripts/php.sh
+++ scripts/php.sh
@@ -66,14 +66,14 @@ zend_loader.obfuscation_level_support=3
 zend_loader.license_path=
 
 ;opcache
-[Zend Opcache]
-zend_extension=/usr/local/zend/opcache.so
-opcache.memory_consumption=128
-opcache.interned_strings_buffer=8
-opcache.max_accelerated_files=4000
-opcache.revalidate_freq=60
-opcache.fast_shutdown=1
-opcache.enable_cli=1
+;[Zend Opcache]
+;zend_extension=/usr/local/zend/opcache.so
+;opcache.memory_consumption=128
+;opcache.interned_strings_buffer=8
+;opcache.max_accelerated_files=4000
+;opcache.revalidate_freq=60
+;opcache.fast_shutdown=1
+;opcache.enable_cli=1
 ;opcache end
 
 ;xcache
