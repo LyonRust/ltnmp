2105f3122843900eda7fccff6c68208718147b3f
Andy <php360@qq.com>
2015-08-26 08:13:53 +0800

    更新Tengine到2.1.0

--- scripts/tengine.sh
+++ scripts/tengine.sh
@@ -3,15 +3,15 @@
 install_tengine() {
     echo "--------------------------------------------"
     echo ""
-    echo "     Install Tengine-2.1.0"
+    echo "     Install Tengine-2.1.1"
     echo ""
     echo "     By:Andy http://www.moqifei.com"
     echo ""
     echo "--------------------------------------------"
 
     cd ${current_dir}/src
-    tar -zxvf tengine-2.1.0.tar.gz
-    cd tengine-2.1.0
+    tar -zxvf tengine-2.1.1.tar.gz
+    cd tengine-2.1.1
     ./configure --user=www --group=www --prefix=/usr/local/nginx --with-http_stub_status_module --with-http_ssl_module --with-http_spdy_module --with-http_gzip_static_module --with-ipv6 --with-http_sub_module --with-http_sysguard_module --with-http_concat_module --with-jemalloc
     make && make install
 
