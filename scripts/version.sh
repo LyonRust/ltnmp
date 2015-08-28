e0074deec8e4a304837dce47e6404c5d5fdb3fc6
Andy <php360@qq.com>
2015-08-28 16:17:01 +0800

    mariadb更新至10.0.21stable，yaw更新至2.3.4stable

--- scripts/version.sh
+++ scripts/version.sh
@@ -6,7 +6,7 @@ ltnmp_tengine='tengine-2.1.1'
 ltnmp_nginx='nginx-1.9.4'
 ltnmp_php='php-5.6.12'
 ltnmp_phpmyadmin='phpMyAdmin-4.4.14-all-languages'
-ltnmp_mariadb='mariadb-10.0.20'
+ltnmp_mariadb='mariadb-10.0.21'
 ltnmp_mysql='mysql-5.6.26'
 
 # 附加模块版本
@@ -14,7 +14,7 @@ ltnmp_swoole='swoole-1.7.18-stable'
 ltnmp_phalcon='phalcon-v2.0.7'
 ltnmp_redis='redis-3.0.3'
 ltnmp_php_redis='redis-2.2.7'
-ltnmp_yaf='yaf-2.3.3'
+ltnmp_yaf='yaf-2.3.4'
 
 # 系统组件
 ltnmp_autoconf='autoconf-2.13'
