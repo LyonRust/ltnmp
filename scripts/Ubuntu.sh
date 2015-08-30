ed57d8093f29f20ea480e510d5237e658c7db7f6
Andy <php360@qq.com>
2015-08-30 15:46:11 +0800

    Ubuntu系统安装脚本添加

---
+++ scripts/Ubuntu.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+install() {
+    # 初始化工作
+    dispaly_selection
+
+    # 系统编译组件检测、安装
+    check_system
+
+    # 安装系统组件/依赖
+    install_system_dependence
+
+    # 添加用户(组)
+    add_user
+
+    # 安装mariadb10.0.20
+    if [ "${install_mariadb}" == "y" ] ; then
+        install_mariadb
+    else
+        install_mysql
+    fi
+
+    # 安装php-5.6.12
+    install_php
+
+    # 安装phpmyadmin
+    install_phpmyadmin
+
+    if [ "${install_tengine}" == "y" ] ; then
+        # 安装tengine
+        install_tengine
+    else
+        # 安装Nginx
+        install_nginx
+    fi
+
+    # 系统组件还原
+    end_system
+}
+
+# 系统编译组件检测、安装
+check_system() {
+    cat /etc/issue
+    uname -a
+    MemTotal=`free -m | grep Mem | awk '{print  $2}'`
+    echo -e "\n Memory is: ${MemTotal} MB "
+
+    # 设置时区
+    rm -rf /etc/localtime
+    ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+    apt-get install -y ntpdate
+    ntpdate -u pool.ntp.org
+    date
+
+    apt-get update
+    apt-get remove -y apache2 apache2-doc apache2-utils apache2.2-common apache2.2-bin apache2-mpm-prefork apache2-doc apache2-mpm-worker mysql-client mysql-server mysql-common php5 php5-common php5-cgi php5-mysql php5-curl php5-gd
+    killall apache2
+
+    dpkg -l |grep mysql
+    dpkg -P libmysqlclient15off libmysqlclient15-dev mysql-common
+    dpkg -l |grep apache
+    dpkg -P apache2 apache2-doc apache2-mpm-prefork apache2-utils apache2.2-common
+    dpkg -l |grep php
+    dpkg -P php5 php5-common php5-cgi php5-mysql php5-curl php5-gd
+    apt-get purge `dpkg -l | grep php| awk '{print $2}'`
+
+    # 关闭Selinux
+    if [ -s /etc/selinux/config ]; then
+        sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+    fi
+
+    if [ -s /etc/ld.so.conf.d/libc6-xen.conf ]; then
+        sed -i 's/hwcap 1 nosegneg/hwcap 0 nosegneg/g' /etc/ld.so.conf.d/libc6-xen.conf
+    fi
+
+    echo "apt-get installing dependent packages..."
+    apt-get update -y
+    apt-get autoremove -y
+    apt-get -fy install
+    export DEBIAN_FRONTEND=noninteractive
+    for packages in build-essential gcc g++ make cmake autoconf automake re2c wget cron bzip2 libzip-dev libc6-dev file rcconf flex vim bison m4 gawk less cpp binutils diffutils unzip tar bzip2 libbz2-dev libncurses5 libncurses5-dev libtool libevent-dev openssl libssl-dev zlibc libsasl2-dev libltdl3-dev libltdl-dev zlib1g zlib1g-dev libbz2-1.0 libbz2-dev libglib2.0-0 libglib2.0-dev libpng3 libjpeg62 libjpeg62-dev libjpeg-dev libpng-dev libpng12-0 libpng12-dev curl libcurl3 libcurl3-gnutls libcurl4-gnutls-dev libcurl4-openssl-dev libpq-dev libpq5 gettext libjpeg-dev libpng12-dev libxml2-dev libcap-dev ca-certificates debian-keyring debian-archive-keyring libc-client2007e-dev psmisc patch git libc-ares-dev;
+    do apt-get install -y $packages --force-yes; done
+}
+
+ubuntu_modify_source()
+{
+    code_name=''
+    if grep -Eqi "10.10" /etc/*-release || echo "${Ubuntu_Version}" | grep -Eqi '^10.10'; then
+        code_name='maverick'
+    elif grep -Eqi "11.04" /etc/*-release || echo "${Ubuntu_Version}" | grep -Eqi '^11.04'; then
+        code_name='natty'
+    elif  grep -Eqi "11.10" /etc/*-release || echo "${Ubuntu_Version}" | grep -Eqi '^11.10'; then
+        code_name='oneiric'
+    elif grep -Eqi "12.10" /etc/*-release || echo "${Ubuntu_Version}" | grep -Eqi '^12.10'; then
+        code_name='quantal'
+    elif grep -Eqi "13.04" /etc/*-release || echo "${Ubuntu_Version}" | grep -Eqi '^13.04'; then
+        code_name='raring'
+    elif grep -Eqi "13.10" /etc/*-release || echo "${Ubuntu_Version}" | grep -Eqi '^13.10'; then
+        code_name='saucy'
+    elif grep -Eqi "10.04" /etc/*-release || echo "${Ubuntu_Version}" | grep -Eqi '^10.04'; then
+        code_name='lucid'
+    elif grep -Eqi "14.10" /etc/*-release || echo "${Ubuntu_Version}" | grep -Eqi '^14.10'; then
+        Ubuntu_Deadline
+    elif grep -Eqi "15.04" /etc/*-release || echo "${Ubuntu_Version}" | grep -Eqi '^15.04'; then
+        Ubuntu_Deadline
+    elif grep -Eqi "12.04" /etc/*-release || echo "${Ubuntu_Version}" | grep -Eqi '^12.04'; then
+        Ubuntu_Deadline
+    fi
+    if [ "${code_name}" != "" ]; then
+        cp /etc/apt/sources.list /etc/apt/sources.list.$(date +"%Y%m%d")
+        cat > /etc/apt/sources.list<<EOF
+deb http://old-releases.ubuntu.com/ubuntu/ ${code_name} main restricted universe multiverse
+deb http://old-releases.ubuntu.com/ubuntu/ ${code_name}-security main restricted universe multiverse
+deb http://old-releases.ubuntu.com/ubuntu/ ${code_name}-updates main restricted universe multiverse
+deb http://old-releases.ubuntu.com/ubuntu/ ${code_name}-proposed main restricted universe multiverse
+deb http://old-releases.ubuntu.com/ubuntu/ ${code_name}-backports main restricted universe multiverse
+deb-src http://old-releases.ubuntu.com/ubuntu/ ${code_name} main restricted universe multiverse
+deb-src http://old-releases.ubuntu.com/ubuntu/ ${code_name}-security main restricted universe multiverse
+deb-src http://old-releases.ubuntu.com/ubuntu/ ${code_name}-updates main restricted universe multiverse
+deb-src http://old-releases.ubuntu.com/ubuntu/ ${code_name}-proposed main restricted universe multiverse
+deb-src http://old-releases.ubuntu.com/ubuntu/ ${code_name}-backports main restricted universe multiverse
+EOF
+    fi
+}
