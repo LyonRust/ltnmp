10cdc53e90c32ed12e00bd2e26d4cab112701600
Andy <php360@qq.com>
2015-11-20 11:08:03 +0800

    mysql版本更新到5.6.27

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,7 +7,7 @@ ltnmp_nginx='nginx-1.9.7'
 ltnmp_php='php-5.6.15'
 ltnmp_phpmyadmin='phpMyAdmin-4.4.14-all-languages'
 ltnmp_mariadb='mariadb-10.0.22'
-ltnmp_mysql='mysql-5.6.26'
+ltnmp_mysql='mysql-5.6.27'
 
 # 附加模块版本
 ltnmp_swoole='swoole-1.7.19'
