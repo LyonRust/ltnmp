3517318fd4cfb0a5b7a7c1162b636c689eb2cf91
Andy <php360@qq.com>
2015-11-20 17:09:13 +0800

    --without-dso

--- scripts/tengine.sh
+++ scripts/tengine.sh
@@ -12,7 +12,7 @@ install_tengine() {
     cd ${current_dir}/src
     tar -zxvf ${ltnmp_tengine}.tar.gz
     cd ${ltnmp_tengine}
-    ./configure --user=www --group=www --prefix=/usr/local/nginx --with-http_stub_status_module --with-http_ssl_module --with-http_spdy_module --with-http_gzip_static_module --with-ipv6 --with-http_sub_module --with-http_sysguard_module --with-http_concat_module --with-jemalloc
+    ./configure --user=www --group=www --prefix=/usr/local/nginx --with-http_stub_status_module --with-http_ssl_module --with-http_spdy_module --with-http_gzip_static_module --with-ipv6 --with-http_sub_module --with-http_sysguard_module --with-http_concat_module --with-jemalloc --without-dso
     make && make install
 
     ln -s /usr/local/nginx/sbin/nginx /usr/bin/nginx
